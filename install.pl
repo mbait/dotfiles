a949515ea2358e3ad7e39d1b99976263e9fe7105
Alexander Solovets <asolovets@gmail.com>
2011-04-24 17:42:39 +1100

    script for automated installation of configuration files

---
+++ install.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Cwd;
+
+open FH, "files.path";
+my %list;
+while (<FH>) {
+	my ($file, $path) = split /\s+/;
+	$path ||= '~';
+	$list{$file} = $path;
+}
+close FH;
+
+my $cwd = getcwd;
+while (<.*>) {
+	if (exists $list{$_}) {
+		system "ln -s $cwd/$_ $list{$_}";
+	} else {
+		warn "Skip '$_' - has no corresponding path";
+	}
+}
